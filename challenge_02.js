aa6549a8b6ec1c25c54ff58fa39f715f086e9449
RiKo <ri.ko@outlook.com>
2021-05-19 14:36:57 +0200

    lösung

--- challenge_02.js
+++ challenge_02.js
@@ -15,3 +15,48 @@ Alter 13-17 // Cola
 */
 
 /* Getränke-Challenge */
+
+let name1, age, drink;
+
+name1 = prompt("insert name");
+age = prompt("insert age");
+/*
+if (age < 6)
+{
+    drink = "Milch";
+}
+else if (age < 13)
+{
+    drink = "Saft";
+}
+else if (age < 18)
+{
+    drink = "Cola";
+}
+else
+{
+    drink = "Wein";
+}
+
+console.log(name1 + " gets " + drink);
+*/
+
+switch (true) {
+    case (age < 6):
+        drink = "Milch";
+        break;
+    case (age < 13):
+        drink = "Saft";
+        break;
+    case (age < 18):
+        drink = "Cola";
+        break;
+    case (age > 17):
+        drink = "Wein";
+        break;
+
+    default:
+        drink = "Bekommt nix"
+        break;
+}
+console.log(name1 + " gets a" + drink);
